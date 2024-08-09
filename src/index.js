7b29611dbd1f7673a0d6c19047417e56afdae447
toykam <kabirtoyyib19@gmail.com>
2024-08-09 08:41:51 +0100

    add

---
+++ src/index.js
@@ -0,0 +1,81 @@
+import { PrismaClient } from "@prisma/client";
+import dns from "dns";
+import express from "express";
+import Joi from "joi";
+import { nanoid } from "nanoid";
+
+const app = express();
+
+
+app.use(express.urlencoded({
+    extended: false
+}))
+app.use(express.json())
+app.use(express.static("src/public"))
+
+// db config
+const db = new PrismaClient();
+
+app.post("/shorten-url", async (req, res) => {
+    let {url, userId} = req.body;
+    const schema = Joi.object({
+        url: Joi.string().required().uri(),
+        userId: Joi.string()
+    })
+    const { error, value } = schema.validate(req.body);
+    if (error != null) {
+        return res.status(400).json({
+            "message": error.message
+        })
+    }
+    // return dns.lookup(url, async (err) => {
+    //     if (err) {
+    //         console.log(err)
+    //         return res.status(500).json({"message": "domain does not exist."})
+    //     }
+    // })
+    let existing = await db.history.findFirst({
+        where: {originalUrl: url}
+    })
+    if (existing == null) { // which means does not exist
+        // create
+        const code = nanoid(4) // create a new code for the new url
+        const host = req.get("host")
+        if (userId == null) userId = nanoid(10)
+        existing = await db.history.create({
+            data: {
+                originalUrl: value.url,
+                shortUrl: `${host}/${code}`,
+                code: code,
+                userId: userId
+            }
+        })
+    }
+    return res.json(existing)
+
+})
+
+app.get("/:code", async (req, res) => {
+    const shorten = await db.history.findFirst({
+        where: {
+            code: req.params.code
+        }
+    })
+    if (shorten == null) {
+        return res.send("Url does not existing")
+    }
+
+    res.redirect(shorten.originalUrl);
+})
+app.get("/history/:userId", async (req, res) => {
+    const shortenUrls = await db.history.findMany({
+        where: {
+            userId: req.params.userId
+        }
+    })
+
+    res.json(shortenUrls);
+})
+
+
+app.listen(4000, () => console.log('Server started at 4000'))
---
+++ src/public/js/script.js
@@ -0,0 +1,159 @@
+const token = "503bf5006708dc769617c571f07206858ef8c018";
+const button = document.querySelector("#shorten");
+const input = document.querySelector("#input-field");
+const longUrl = document.querySelector("#input-url");
+const shortUrl = document.querySelector("#new-url");
+const resultDiv = document.querySelector("#output-div")
+const errorDiv = document.querySelector("#error-div");
+const errorMessage = document.querySelector("#error-text");
+const clearButton = document.querySelector("#clear-btn");
+const copyButton = document.querySelector("#copy-btn");
+const shortHistoryList = document.querySelector("#short-history");
+
+/* button action */
+button.addEventListener("click", (event) => {
+  event.preventDefault();
+  if(input.value) {
+    shorten(input.value);
+  } else {
+    showError();
+    hideResult();
+  }
+})
+
+/* function to handle errors */
+const handleError = (response) => {
+  console.log(response);
+//   response
+  if(!response.ok) {
+    
+    showError("Please enter a valid URL.");
+    hideResult();
+  } else {
+  hideError();
+  return response;
+  }
+}
+
+/* function to get shortened url with input "url" with fetch and deal with error */
+const shorten = (input) => {
+    // console.log(input)
+    shortUrl.innerHTML = "Please wait...";
+    showResult()
+    const userId = localStorage.getItem("user_id")
+    fetch("/shorten-url", {
+        method: "POST",
+        headers: {
+            //   "Authorization": `Bearer ${token}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "url": input,
+            "userId": userId
+        }),
+    })
+    .then(handleError)
+    .then(response => response.json())
+    .then((json) => {
+        localStorage.setItem('user_id', json.userId);
+        shortUrl.innerHTML = json.shortUrl;
+        getUpdated()
+        showResult();
+    })
+    .catch(error => {
+        console.log(error);
+    })
+}
+
+const getUpdated = () => {
+    const userId = localStorage.getItem('user_id')
+    fetch("/history/"+userId, {
+        method: "GET"
+    })
+    .then((v) => v.json())
+    .then((v) => {
+        console.log(v)
+        localStorage.setItem('history', JSON.stringify(v))
+        showHistory(v);
+    })
+}
+
+/* Clipboard functions */
+
+// const clipboard = new ClipboardJS("#copy-btn");
+
+// clipboard.on('success', function(e) {
+//     console.info('Action:', e.action);
+//     console.info('Text:', e.text);
+//     console.info('Trigger:', e.trigger);
+
+//     e.clearSelection();
+// });
+
+// clipboard.on('error', function(e) {
+//     console.error('Action:', e.action);
+//     console.error('Trigger:', e.trigger);
+// });
+
+/* Clear fields */
+const clearFields = () => {
+  input.value = '';
+  hideResult();
+  hideError();
+}
+
+clearButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  clearFields();
+})
+
+
+/* display/hide results and errors */
+const showResult = () => {
+  shortUrl.style.display = "flex";
+  hideError()
+}
+
+const hideResult = () => {
+  shortUrl.style.display = "none";
+}
+
+const showError = (message) => {
+    errorMessage.textContent = message
+    errorDiv.style.display = "block";
+    hideResult()
+}
+
+const hideError = () => {
+  errorDiv.style.display = "none";
+}
+
+const showHistory = () => {
+    let history = localStorage.getItem("history")
+    const his = JSON.parse(history)
+    console.log(his);
+
+    shortHistoryList.innerHTML = "";
+
+    for (let index = 0; index < his.length; index++) {
+        const short = his[index];
+        const element = document.createElement("li")
+        const aElement = document.createElement("a")
+
+        aElement.setAttribute("href", short['shortUrl'])
+        aElement.setAttribute("target","__blank")
+
+        aElement.innerHTML = short['shortUrl']
+
+        element.setAttribute("class", "short_history")
+
+        element.appendChild(aElement)
+
+        element.style = "color: white"
+
+        shortHistoryList.appendChild(element);
+    }
+
+}
+
+getUpdated();
